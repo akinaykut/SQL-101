72ac1626e6f02ebb5c8341874c52174569b0811b
Akin Aykut <akinaykut05@gmail.com>
2022-10-04 16:50:47 +0300

    Added task-9 solution.

---
+++ sql_task-9.sql
@@ -0,0 +1,8 @@
+-- SELECT city, country FROM city, country 
+-- WHERE city.city_id = country.country_id;
+
+-- SELECT payment_id, first_name, last_name FROM customer, payment
+-- WHERE customer.customer_id = payment.customer_id;
+
+-- SELECT rental_id, first_name, last_name FROM customer, rental
+-- WHERE customer.customer_id = rental.customer_id;
