edc95307dcb54f8e352db5d6ca1935ef71548c4b
Akin Aykut <akinaykut05@gmail.com>
2022-10-04 14:44:41 +0300

    Added task-8 solution.

---
+++ sql_task-8.sql
@@ -0,0 +1,33 @@
+-- UPDATE employee
+-- SET name = 'Aykut'
+-- WHERE name LIKE 'A%';
+
+-- UPDATE employee
+-- SET id = 250
+-- WHERE email LIKE '%pivanchov%';
+
+-- UPDATE employee
+-- SET name = 'Jesus'
+-- WHERE birthday < '2002-03-10';
+
+
+-- UPDATE employee
+-- SET birthday = '1997-09-08'
+-- WHERE id = 7;
+
+
+-- DELETE FROM employee
+-- WHERE name = 'Jesus';
+
+-- DELETE FROM employee
+-- WHERE id = 2;
+
+-- DELETE FROM employe
+-- WHERE birthday = '2019-01-25';
+
+-- DELETE FROM employe
+-- WHERE email LIKE '%nabdie%';
+
+
+
+
