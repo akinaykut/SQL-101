0893943e426598202a922bd774248b2b9fe18358
Akin Aykut <akinaykut05@gmail.com>
2022-10-04 09:59:03 +0300

    Update sql_task-3.sql

--- sql_task-3.sql
+++ sql_task-3.sql
@@ -2,6 +2,6 @@
 
 -- SELECT * from country WHERE country LIKE '%n' AND LENGTH(country) > 5;
 
--- SELECT * from film WHERE title LIKE '%T%T%T%T%';
+-- SELECT * from film WHERE title ILIKE '%T%T%T%T%';
 
 -- SELECT * from film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
