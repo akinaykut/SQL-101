31ae79b08861482e91347611ccc7b78940f01a9a
Akin Aykut <akinaykut05@gmail.com>
2022-10-04 09:57:12 +0300

    Added task-3 solution.

---
+++ sql_task-3.sql
@@ -0,0 +1,7 @@
+-- SELECT * from country WHERE country LIKE 'A%a';
+
+-- SELECT * from country WHERE country LIKE '%n' AND LENGTH(country) > 5;
+
+-- SELECT * from film WHERE title LIKE '%T%T%T%T%';
+
+-- SELECT * from film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
